c54c360c08883d60ec6edcd48025af74bc639c38
Luvkush <luvkush.paroksya@gmail.com>
2026-09-14 13:10:21 +0530

    Add complete HealthPilot iOS project

---
+++ ios/HealthPilot/HealthPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPilotApp.swift
+//  HealthPilot
+//
+//  Created by paroksya on 05/08/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
